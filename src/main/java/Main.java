49dc8011883e7589cdd529341d9a48c553448354
Arnoud-Wilson <arnoud608@gmail.com>
2023-05-20 16:34:08 +0200

    test bug fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,13 @@
 
 public class Main {
     public static void main(String[] args) {
-        Car fiat500 = new Car("fiat", "500", "2019", "groen", "1.4");
-        Car volvoCX90 = new Car("Volvo", "CX90", "2022", "zwart", "2.0");
         Car toyotaAygo = new Car("Toyota", "Aygo", "2016", "blauw", "1.2");
+        Car fiat500 = new Car("Fiat", "500", "2019", "groen", "1.4");
+        Car volvoCX90 = new Car("Volvo", "CX90", "2022", "zwart", "2.0");
 
+        toyotaAygo.printCarStatistics();
         fiat500.printCarStatistics();
         volvoCX90.printCarStatistics();
-        toyotaAygo.printCarStatistics();
+
     }
 }
